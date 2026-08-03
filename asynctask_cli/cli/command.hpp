1eff839c9c782398cbcef9e3677b7371075ff47a
Nijat Ibrahimov <ibrahimovnijat@gmail.com>
2026-08-03 09:15:18 +0200

    Remove redundant func

--- asynctask_cli/cli/command.hpp
+++ asynctask_cli/cli/command.hpp
@@ -19,38 +19,6 @@ struct Command {
         Invalid, // Error describes the problem.
     };
 
-    std::string_view typeToString(Type type) {
-        if (type == Type::None)
-            return "None";
-        else if (type == Type::Help) {
-            return "Help";
-        }
-        else if (type == Type::Quit) {
-            return "Quit";
-        }
-        else if (type == Type::Start) {
-            return "Start";
-        }
-        else if (type == Type::Stop) {
-            return "Stop";
-        }
-        else if (type == Type::Pause) {
-            return "Pause";
-        }
-        else if (type == Type::Resume) {
-            return "Resume";
-        }
-        else if (type == Type::Status) {
-            return "Status";
-        }
-        else if (type == Type::Invalid) {
-            return "Invalid";
-        }
-        else {
-            return "Unknown";
-        }
-    }
-
     Type type = Type::None;
     std::optional<std::string> arg;  // Optionally, have arguments too
     std::optional<std::string> error;
